bfaffd3a79132921dc3c6590b284ed3ad926ae3b
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2024-10-07 22:20:24 -0400

    02_repeatString
    Solved exercise using for loop to repeat the string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(inputString, num) {
 
+    if(num < 0)
+        return `ERROR`;
+    let result = ``;
+    for(let i = 0; i < num; i++)
+        result += inputString;
+
+    return result;
 };
 
 // Do not edit below this line
