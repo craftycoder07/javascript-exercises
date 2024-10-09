0c6d219728bcf59c87fa2dc6851faf20b59ab84f
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2024-10-08 22:00:05 -0400

    03_reverseString
    Implemented method to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let stringToReverseLength = stringToReverse.length;
+    let reversedString = ``;
+    for(let i = stringToReverseLength - 1; i >= 0; i--)
+    {
+        reversedString += stringToReverse.charAt(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
