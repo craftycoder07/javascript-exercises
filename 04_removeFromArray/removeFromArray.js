823c4126630615300df228603c231cfe4df94ed8
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2024-10-08 22:24:24 -0400

    04_removeFromArray
    Implemented a function that removes given elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArr, ...theArgs) {
+    let resultArr = [];
+    for (let i = 0; i < originalArr.length; i++)
+    {
+        let isPresent = false;
+        for(let j = 0; j < theArgs.length; j++)
+        {
+            if(originalArr[i] === theArgs[j])
+                isPresent = true;
+        }
+
+        if(isPresent === false)
+            resultArr.push(originalArr[i]);
+    }
+    return resultArr;
 };
 
 // Do not edit below this line
