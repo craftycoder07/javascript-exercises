3b74518db450087c14445807f126a93f4ebd6d36
craftycoder07 <22353899+craftycoder07@users.noreply.github.com>
2024-10-09 23:21:29 -0400

    05_sumAll
    Implemented a method that adds all numbers between two input numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if(num1 < 0 || num2 < 0)
+        return `ERROR`;
+
+    if(Number.isInteger(num1) === false || Number.isInteger(num2) === false)
+        return `ERROR`;
+    
+    let result = 0;
+    let smallerNumber = (num1 < num2) ? num1 : num2;
+    let largerNumber = (num1 > num2) ? num1 : num2;
+    while(smallerNumber <= largerNumber)
+    {
+        result += smallerNumber;
+        smallerNumber++;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
